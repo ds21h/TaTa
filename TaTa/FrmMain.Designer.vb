2cf17bb3b76644f0823d12404fb91fd6e5f81844
Jan van den Broek <ds21h@hotmail.com>
2018-05-19 16:09:16 +0200

    First version

---
+++ TaTa/FrmMain.Designer.vb
@@ -0,0 +1,562 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+  Inherits System.Windows.Forms.Form
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+    Try
+      If disposing AndAlso components IsNot Nothing Then
+        components.Dispose()
+      End If
+    Finally
+      MyBase.Dispose(disposing)
+    End Try
+  End Sub
+
+  'Required by the Windows Form Designer
+  Private components As System.ComponentModel.IContainer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> _
+  Private Sub InitializeComponent()
+    Me.LbWidth = New System.Windows.Forms.Label()
+    Me.TxtStand = New System.Windows.Forms.TextBox()
+    Me.Label1 = New System.Windows.Forms.Label()
+    Me.Label2 = New System.Windows.Forms.Label()
+    Me.Label3 = New System.Windows.Forms.Label()
+    Me.Label4 = New System.Windows.Forms.Label()
+    Me.Label5 = New System.Windows.Forms.Label()
+    Me.Label6 = New System.Windows.Forms.Label()
+    Me.Label7 = New System.Windows.Forms.Label()
+    Me.Label8 = New System.Windows.Forms.Label()
+    Me.Label9 = New System.Windows.Forms.Label()
+    Me.Label10 = New System.Windows.Forms.Label()
+    Me.TxtNumb1 = New System.Windows.Forms.TextBox()
+    Me.TxtWidth1 = New System.Windows.Forms.TextBox()
+    Me.TxtNumb2 = New System.Windows.Forms.TextBox()
+    Me.TxtWidth2 = New System.Windows.Forms.TextBox()
+    Me.TxtNumb3 = New System.Windows.Forms.TextBox()
+    Me.TxtWidth3 = New System.Windows.Forms.TextBox()
+    Me.TxtNumb4 = New System.Windows.Forms.TextBox()
+    Me.TxtWidth4 = New System.Windows.Forms.TextBox()
+    Me.TxtNumb5 = New System.Windows.Forms.TextBox()
+    Me.TxtWidth5 = New System.Windows.Forms.TextBox()
+    Me.TxtNumb6 = New System.Windows.Forms.TextBox()
+    Me.TxtWidth6 = New System.Windows.Forms.TextBox()
+    Me.TxtNumb7 = New System.Windows.Forms.TextBox()
+    Me.TxtWidth7 = New System.Windows.Forms.TextBox()
+    Me.TxtNumb8 = New System.Windows.Forms.TextBox()
+    Me.TxtWidth8 = New System.Windows.Forms.TextBox()
+    Me.TxtNumb9 = New System.Windows.Forms.TextBox()
+    Me.TxtWidth9 = New System.Windows.Forms.TextBox()
+    Me.TxtNumb0 = New System.Windows.Forms.TextBox()
+    Me.TxtWidth0 = New System.Windows.Forms.TextBox()
+    Me.LbTot1 = New System.Windows.Forms.Label()
+    Me.LbTot2 = New System.Windows.Forms.Label()
+    Me.LbTot3 = New System.Windows.Forms.Label()
+    Me.LbTot4 = New System.Windows.Forms.Label()
+    Me.LbTot5 = New System.Windows.Forms.Label()
+    Me.LbTot6 = New System.Windows.Forms.Label()
+    Me.LbTot7 = New System.Windows.Forms.Label()
+    Me.LbTot8 = New System.Windows.Forms.Label()
+    Me.LbTot9 = New System.Windows.Forms.Label()
+    Me.LbTot0 = New System.Windows.Forms.Label()
+    Me.BtnCalc = New System.Windows.Forms.Button()
+    Me.Label11 = New System.Windows.Forms.Label()
+    Me.LbTot = New System.Windows.Forms.Label()
+    Me.Label12 = New System.Windows.Forms.Label()
+    Me.LbFraction = New System.Windows.Forms.Label()
+    Me.SuspendLayout()
+    '
+    'LbWidth
+    '
+    Me.LbWidth.AutoSize = True
+    Me.LbWidth.Location = New System.Drawing.Point(12, 20)
+    Me.LbWidth.Name = "LbWidth"
+    Me.LbWidth.Size = New System.Drawing.Size(83, 13)
+    Me.LbWidth.TabIndex = 0
+    Me.LbWidth.Text = "Standard bredd:"
+    '
+    'TxtStand
+    '
+    Me.TxtStand.Location = New System.Drawing.Point(101, 17)
+    Me.TxtStand.Name = "TxtStand"
+    Me.TxtStand.Size = New System.Drawing.Size(100, 20)
+    Me.TxtStand.TabIndex = 1
+    Me.TxtStand.Text = "1230"
+    '
+    'Label1
+    '
+    Me.Label1.AutoSize = True
+    Me.Label1.Location = New System.Drawing.Point(288, 36)
+    Me.Label1.Name = "Label1"
+    Me.Label1.Size = New System.Drawing.Size(18, 13)
+    Me.Label1.TabIndex = 2
+    Me.Label1.Text = "x1"
+    '
+    'Label2
+    '
+    Me.Label2.AutoSize = True
+    Me.Label2.Location = New System.Drawing.Point(288, 61)
+    Me.Label2.Name = "Label2"
+    Me.Label2.Size = New System.Drawing.Size(18, 13)
+    Me.Label2.TabIndex = 3
+    Me.Label2.Text = "x2"
+    '
+    'Label3
+    '
+    Me.Label3.AutoSize = True
+    Me.Label3.Location = New System.Drawing.Point(288, 86)
+    Me.Label3.Name = "Label3"
+    Me.Label3.Size = New System.Drawing.Size(18, 13)
+    Me.Label3.TabIndex = 4
+    Me.Label3.Text = "x3"
+    '
+    'Label4
+    '
+    Me.Label4.AutoSize = True
+    Me.Label4.Location = New System.Drawing.Point(288, 111)
+    Me.Label4.Name = "Label4"
+    Me.Label4.Size = New System.Drawing.Size(18, 13)
+    Me.Label4.TabIndex = 5
+    Me.Label4.Text = "x4"
+    '
+    'Label5
+    '
+    Me.Label5.AutoSize = True
+    Me.Label5.Location = New System.Drawing.Point(288, 136)
+    Me.Label5.Name = "Label5"
+    Me.Label5.Size = New System.Drawing.Size(18, 13)
+    Me.Label5.TabIndex = 6
+    Me.Label5.Text = "x5"
+    '
+    'Label6
+    '
+    Me.Label6.AutoSize = True
+    Me.Label6.Location = New System.Drawing.Point(288, 161)
+    Me.Label6.Name = "Label6"
+    Me.Label6.Size = New System.Drawing.Size(18, 13)
+    Me.Label6.TabIndex = 7
+    Me.Label6.Text = "x6"
+    '
+    'Label7
+    '
+    Me.Label7.AutoSize = True
+    Me.Label7.Location = New System.Drawing.Point(288, 211)
+    Me.Label7.Name = "Label7"
+    Me.Label7.Size = New System.Drawing.Size(18, 13)
+    Me.Label7.TabIndex = 8
+    Me.Label7.Text = "x8"
+    '
+    'Label8
+    '
+    Me.Label8.AutoSize = True
+    Me.Label8.Location = New System.Drawing.Point(288, 186)
+    Me.Label8.Name = "Label8"
+    Me.Label8.Size = New System.Drawing.Size(18, 13)
+    Me.Label8.TabIndex = 9
+    Me.Label8.Text = "x7"
+    '
+    'Label9
+    '
+    Me.Label9.AutoSize = True
+    Me.Label9.Location = New System.Drawing.Point(288, 236)
+    Me.Label9.Name = "Label9"
+    Me.Label9.Size = New System.Drawing.Size(18, 13)
+    Me.Label9.TabIndex = 10
+    Me.Label9.Text = "x9"
+    '
+    'Label10
+    '
+    Me.Label10.AutoSize = True
+    Me.Label10.Location = New System.Drawing.Point(288, 261)
+    Me.Label10.Name = "Label10"
+    Me.Label10.Size = New System.Drawing.Size(24, 13)
+    Me.Label10.TabIndex = 11
+    Me.Label10.Text = "x10"
+    '
+    'TxtNumb1
+    '
+    Me.TxtNumb1.Location = New System.Drawing.Point(312, 58)
+    Me.TxtNumb1.Name = "TxtNumb1"
+    Me.TxtNumb1.Size = New System.Drawing.Size(78, 20)
+    Me.TxtNumb1.TabIndex = 12
+    '
+    'TxtWidth1
+    '
+    Me.TxtWidth1.Location = New System.Drawing.Point(406, 58)
+    Me.TxtWidth1.Name = "TxtWidth1"
+    Me.TxtWidth1.Size = New System.Drawing.Size(78, 20)
+    Me.TxtWidth1.TabIndex = 13
+    '
+    'TxtNumb2
+    '
+    Me.TxtNumb2.Location = New System.Drawing.Point(312, 83)
+    Me.TxtNumb2.Name = "TxtNumb2"
+    Me.TxtNumb2.Size = New System.Drawing.Size(78, 20)
+    Me.TxtNumb2.TabIndex = 14
+    '
+    'TxtWidth2
+    '
+    Me.TxtWidth2.Location = New System.Drawing.Point(406, 83)
+    Me.TxtWidth2.Name = "TxtWidth2"
+    Me.TxtWidth2.Size = New System.Drawing.Size(78, 20)
+    Me.TxtWidth2.TabIndex = 15
+    '
+    'TxtNumb3
+    '
+    Me.TxtNumb3.Location = New System.Drawing.Point(312, 108)
+    Me.TxtNumb3.Name = "TxtNumb3"
+    Me.TxtNumb3.Size = New System.Drawing.Size(78, 20)
+    Me.TxtNumb3.TabIndex = 16
+    '
+    'TxtWidth3
+    '
+    Me.TxtWidth3.Location = New System.Drawing.Point(406, 108)
+    Me.TxtWidth3.Name = "TxtWidth3"
+    Me.TxtWidth3.Size = New System.Drawing.Size(78, 20)
+    Me.TxtWidth3.TabIndex = 17
+    '
+    'TxtNumb4
+    '
+    Me.TxtNumb4.Location = New System.Drawing.Point(312, 133)
+    Me.TxtNumb4.Name = "TxtNumb4"
+    Me.TxtNumb4.Size = New System.Drawing.Size(78, 20)
+    Me.TxtNumb4.TabIndex = 18
+    '
+    'TxtWidth4
+    '
+    Me.TxtWidth4.Location = New System.Drawing.Point(406, 133)
+    Me.TxtWidth4.Name = "TxtWidth4"
+    Me.TxtWidth4.Size = New System.Drawing.Size(78, 20)
+    Me.TxtWidth4.TabIndex = 19
+    '
+    'TxtNumb5
+    '
+    Me.TxtNumb5.Location = New System.Drawing.Point(312, 158)
+    Me.TxtNumb5.Name = "TxtNumb5"
+    Me.TxtNumb5.Size = New System.Drawing.Size(78, 20)
+    Me.TxtNumb5.TabIndex = 20
+    '
+    'TxtWidth5
+    '
+    Me.TxtWidth5.Location = New System.Drawing.Point(406, 158)
+    Me.TxtWidth5.Name = "TxtWidth5"
+    Me.TxtWidth5.Size = New System.Drawing.Size(78, 20)
+    Me.TxtWidth5.TabIndex = 21
+    '
+    'TxtNumb6
+    '
+    Me.TxtNumb6.Location = New System.Drawing.Point(312, 183)
+    Me.TxtNumb6.Name = "TxtNumb6"
+    Me.TxtNumb6.Size = New System.Drawing.Size(78, 20)
+    Me.TxtNumb6.TabIndex = 22
+    '
+    'TxtWidth6
+    '
+    Me.TxtWidth6.Location = New System.Drawing.Point(406, 183)
+    Me.TxtWidth6.Name = "TxtWidth6"
+    Me.TxtWidth6.Size = New System.Drawing.Size(78, 20)
+    Me.TxtWidth6.TabIndex = 23
+    '
+    'TxtNumb7
+    '
+    Me.TxtNumb7.Location = New System.Drawing.Point(312, 208)
+    Me.TxtNumb7.Name = "TxtNumb7"
+    Me.TxtNumb7.Size = New System.Drawing.Size(78, 20)
+    Me.TxtNumb7.TabIndex = 24
+    '
+    'TxtWidth7
+    '
+    Me.TxtWidth7.Location = New System.Drawing.Point(406, 208)
+    Me.TxtWidth7.Name = "TxtWidth7"
+    Me.TxtWidth7.Size = New System.Drawing.Size(78, 20)
+    Me.TxtWidth7.TabIndex = 25
+    '
+    'TxtNumb8
+    '
+    Me.TxtNumb8.Location = New System.Drawing.Point(312, 233)
+    Me.TxtNumb8.Name = "TxtNumb8"
+    Me.TxtNumb8.Size = New System.Drawing.Size(78, 20)
+    Me.TxtNumb8.TabIndex = 26
+    '
+    'TxtWidth8
+    '
+    Me.TxtWidth8.Location = New System.Drawing.Point(406, 233)
+    Me.TxtWidth8.Name = "TxtWidth8"
+    Me.TxtWidth8.Size = New System.Drawing.Size(78, 20)
+    Me.TxtWidth8.TabIndex = 27
+    '
+    'TxtNumb9
+    '
+    Me.TxtNumb9.Location = New System.Drawing.Point(312, 258)
+    Me.TxtNumb9.Name = "TxtNumb9"
+    Me.TxtNumb9.Size = New System.Drawing.Size(78, 20)
+    Me.TxtNumb9.TabIndex = 28
+    '
+    'TxtWidth9
+    '
+    Me.TxtWidth9.Location = New System.Drawing.Point(406, 258)
+    Me.TxtWidth9.Name = "TxtWidth9"
+    Me.TxtWidth9.Size = New System.Drawing.Size(78, 20)
+    Me.TxtWidth9.TabIndex = 29
+    '
+    'TxtNumb0
+    '
+    Me.TxtNumb0.Location = New System.Drawing.Point(312, 33)
+    Me.TxtNumb0.Name = "TxtNumb0"
+    Me.TxtNumb0.Size = New System.Drawing.Size(78, 20)
+    Me.TxtNumb0.TabIndex = 10
+    '
+    'TxtWidth0
+    '
+    Me.TxtWidth0.Location = New System.Drawing.Point(406, 33)
+    Me.TxtWidth0.Name = "TxtWidth0"
+    Me.TxtWidth0.Size = New System.Drawing.Size(78, 20)
+    Me.TxtWidth0.TabIndex = 11
+    '
+    'LbTot1
+    '
+    Me.LbTot1.AutoSize = True
+    Me.LbTot1.Location = New System.Drawing.Point(505, 61)
+    Me.LbTot1.Name = "LbTot1"
+    Me.LbTot1.Size = New System.Drawing.Size(13, 13)
+    Me.LbTot1.TabIndex = 32
+    Me.LbTot1.Text = "0"
+    '
+    'LbTot2
+    '
+    Me.LbTot2.AutoSize = True
+    Me.LbTot2.Location = New System.Drawing.Point(505, 86)
+    Me.LbTot2.Name = "LbTot2"
+    Me.LbTot2.Size = New System.Drawing.Size(13, 13)
+    Me.LbTot2.TabIndex = 33
+    Me.LbTot2.Text = "0"
+    '
+    'LbTot3
+    '
+    Me.LbTot3.AutoSize = True
+    Me.LbTot3.Location = New System.Drawing.Point(505, 111)
+    Me.LbTot3.Name = "LbTot3"
+    Me.LbTot3.Size = New System.Drawing.Size(13, 13)
+    Me.LbTot3.TabIndex = 34
+    Me.LbTot3.Text = "0"
+    '
+    'LbTot4
+    '
+    Me.LbTot4.AutoSize = True
+    Me.LbTot4.Location = New System.Drawing.Point(505, 136)
+    Me.LbTot4.Name = "LbTot4"
+    Me.LbTot4.Size = New System.Drawing.Size(13, 13)
+    Me.LbTot4.TabIndex = 35
+    Me.LbTot4.Text = "0"
+    '
+    'LbTot5
+    '
+    Me.LbTot5.AutoSize = True
+    Me.LbTot5.Location = New System.Drawing.Point(505, 161)
+    Me.LbTot5.Name = "LbTot5"
+    Me.LbTot5.Size = New System.Drawing.Size(13, 13)
+    Me.LbTot5.TabIndex = 36
+    Me.LbTot5.Text = "0"
+    '
+    'LbTot6
+    '
+    Me.LbTot6.AutoSize = True
+    Me.LbTot6.Location = New System.Drawing.Point(505, 186)
+    Me.LbTot6.Name = "LbTot6"
+    Me.LbTot6.Size = New System.Drawing.Size(13, 13)
+    Me.LbTot6.TabIndex = 37
+    Me.LbTot6.Text = "0"
+    '
+    'LbTot7
+    '
+    Me.LbTot7.AutoSize = True
+    Me.LbTot7.Location = New System.Drawing.Point(505, 211)
+    Me.LbTot7.Name = "LbTot7"
+    Me.LbTot7.Size = New System.Drawing.Size(13, 13)
+    Me.LbTot7.TabIndex = 38
+    Me.LbTot7.Text = "0"
+    '
+    'LbTot8
+    '
+    Me.LbTot8.AutoSize = True
+    Me.LbTot8.Location = New System.Drawing.Point(505, 236)
+    Me.LbTot8.Name = "LbTot8"
+    Me.LbTot8.Size = New System.Drawing.Size(13, 13)
+    Me.LbTot8.TabIndex = 39
+    Me.LbTot8.Text = "0"
+    '
+    'LbTot9
+    '
+    Me.LbTot9.AutoSize = True
+    Me.LbTot9.Location = New System.Drawing.Point(505, 261)
+    Me.LbTot9.Name = "LbTot9"
+    Me.LbTot9.Size = New System.Drawing.Size(13, 13)
+    Me.LbTot9.TabIndex = 40
+    Me.LbTot9.Text = "0"
+    '
+    'LbTot0
+    '
+    Me.LbTot0.AutoSize = True
+    Me.LbTot0.Location = New System.Drawing.Point(505, 36)
+    Me.LbTot0.Name = "LbTot0"
+    Me.LbTot0.Size = New System.Drawing.Size(13, 13)
+    Me.LbTot0.TabIndex = 41
+    Me.LbTot0.Text = "0"
+    '
+    'BtnCalc
+    '
+    Me.BtnCalc.Location = New System.Drawing.Point(595, 370)
+    Me.BtnCalc.Name = "BtnCalc"
+    Me.BtnCalc.Size = New System.Drawing.Size(75, 23)
+    Me.BtnCalc.TabIndex = 42
+    Me.BtnCalc.Text = "Calculate"
+    Me.BtnCalc.UseVisualStyleBackColor = True
+    '
+    'Label11
+    '
+    Me.Label11.AutoSize = True
+    Me.Label11.Location = New System.Drawing.Point(309, 307)
+    Me.Label11.Name = "Label11"
+    Me.Label11.Size = New System.Drawing.Size(37, 13)
+    Me.Label11.TabIndex = 43
+    Me.Label11.Text = "Totalt:"
+    '
+    'LbTot
+    '
+    Me.LbTot.AutoSize = True
+    Me.LbTot.Location = New System.Drawing.Point(362, 307)
+    Me.LbTot.Name = "LbTot"
+    Me.LbTot.Size = New System.Drawing.Size(13, 13)
+    Me.LbTot.TabIndex = 44
+    Me.LbTot.Text = "0"
+    '
+    'Label12
+    '
+    Me.Label12.AutoSize = True
+    Me.Label12.Location = New System.Drawing.Point(309, 329)
+    Me.Label12.Name = "Label12"
+    Me.Label12.Size = New System.Drawing.Size(41, 13)
+    Me.Label12.TabIndex = 45
+    Me.Label12.Text = "Behov:"
+    '
+    'LbFraction
+    '
+    Me.LbFraction.AutoSize = True
+    Me.LbFraction.Location = New System.Drawing.Point(362, 329)
+    Me.LbFraction.Name = "LbFraction"
+    Me.LbFraction.Size = New System.Drawing.Size(13, 13)
+    Me.LbFraction.TabIndex = 46
+    Me.LbFraction.Text = "0"
+    '
+    'FrmMain
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(800, 450)
+    Me.Controls.Add(Me.LbFraction)
+    Me.Controls.Add(Me.Label12)
+    Me.Controls.Add(Me.LbTot)
+    Me.Controls.Add(Me.Label11)
+    Me.Controls.Add(Me.BtnCalc)
+    Me.Controls.Add(Me.LbTot0)
+    Me.Controls.Add(Me.LbTot9)
+    Me.Controls.Add(Me.LbTot8)
+    Me.Controls.Add(Me.LbTot7)
+    Me.Controls.Add(Me.LbTot6)
+    Me.Controls.Add(Me.LbTot5)
+    Me.Controls.Add(Me.LbTot4)
+    Me.Controls.Add(Me.LbTot3)
+    Me.Controls.Add(Me.LbTot2)
+    Me.Controls.Add(Me.LbTot1)
+    Me.Controls.Add(Me.TxtWidth0)
+    Me.Controls.Add(Me.TxtNumb0)
+    Me.Controls.Add(Me.TxtWidth9)
+    Me.Controls.Add(Me.TxtNumb9)
+    Me.Controls.Add(Me.TxtWidth8)
+    Me.Controls.Add(Me.TxtNumb8)
+    Me.Controls.Add(Me.TxtWidth7)
+    Me.Controls.Add(Me.TxtNumb7)
+    Me.Controls.Add(Me.TxtWidth6)
+    Me.Controls.Add(Me.TxtNumb6)
+    Me.Controls.Add(Me.TxtWidth5)
+    Me.Controls.Add(Me.TxtNumb5)
+    Me.Controls.Add(Me.TxtWidth4)
+    Me.Controls.Add(Me.TxtNumb4)
+    Me.Controls.Add(Me.TxtWidth3)
+    Me.Controls.Add(Me.TxtNumb3)
+    Me.Controls.Add(Me.TxtWidth2)
+    Me.Controls.Add(Me.TxtNumb2)
+    Me.Controls.Add(Me.TxtWidth1)
+    Me.Controls.Add(Me.TxtNumb1)
+    Me.Controls.Add(Me.Label10)
+    Me.Controls.Add(Me.Label9)
+    Me.Controls.Add(Me.Label8)
+    Me.Controls.Add(Me.Label7)
+    Me.Controls.Add(Me.Label6)
+    Me.Controls.Add(Me.Label5)
+    Me.Controls.Add(Me.Label4)
+    Me.Controls.Add(Me.Label3)
+    Me.Controls.Add(Me.Label2)
+    Me.Controls.Add(Me.Label1)
+    Me.Controls.Add(Me.TxtStand)
+    Me.Controls.Add(Me.LbWidth)
+    Me.Name = "FrmMain"
+    Me.Text = "Bolina production calculation"
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+
+  Friend WithEvents LbWidth As Label
+  Friend WithEvents TxtStand As TextBox
+  Friend WithEvents Label1 As Label
+  Friend WithEvents Label2 As Label
+  Friend WithEvents Label3 As Label
+  Friend WithEvents Label4 As Label
+  Friend WithEvents Label5 As Label
+  Friend WithEvents Label6 As Label
+  Friend WithEvents Label7 As Label
+  Friend WithEvents Label8 As Label
+  Friend WithEvents Label9 As Label
+  Friend WithEvents Label10 As Label
+  Friend WithEvents TxtNumb1 As TextBox
+  Friend WithEvents TxtWidth1 As TextBox
+  Friend WithEvents TxtNumb2 As TextBox
+  Friend WithEvents TxtWidth2 As TextBox
+  Friend WithEvents TxtNumb3 As TextBox
+  Friend WithEvents TxtWidth3 As TextBox
+  Friend WithEvents TxtNumb4 As TextBox
+  Friend WithEvents TxtWidth4 As TextBox
+  Friend WithEvents TxtNumb5 As TextBox
+  Friend WithEvents TxtWidth5 As TextBox
+  Friend WithEvents TxtNumb6 As TextBox
+  Friend WithEvents TxtWidth6 As TextBox
+  Friend WithEvents TxtNumb7 As TextBox
+  Friend WithEvents TxtWidth7 As TextBox
+  Friend WithEvents TxtNumb8 As TextBox
+  Friend WithEvents TxtWidth8 As TextBox
+  Friend WithEvents TxtNumb9 As TextBox
+  Friend WithEvents TxtWidth9 As TextBox
+  Friend WithEvents TxtNumb0 As TextBox
+  Friend WithEvents TxtWidth0 As TextBox
+  Friend WithEvents LbTot1 As Label
+  Friend WithEvents LbTot2 As Label
+  Friend WithEvents LbTot3 As Label
+  Friend WithEvents LbTot4 As Label
+  Friend WithEvents LbTot5 As Label
+  Friend WithEvents LbTot6 As Label
+  Friend WithEvents LbTot7 As Label
+  Friend WithEvents LbTot8 As Label
+  Friend WithEvents LbTot9 As Label
+  Friend WithEvents LbTot0 As Label
+  Friend WithEvents BtnCalc As Button
+  Friend WithEvents Label11 As Label
+  Friend WithEvents LbTot As Label
+  Friend WithEvents Label12 As Label
+  Friend WithEvents LbFraction As Label
+End Class
---
+++ TaTa/FrmMain.vb
@@ -0,0 +1,54 @@
+﻿Public Class FrmMain
+
+  Private Sub BtnCalc_Click(sender As Object, e As EventArgs) Handles BtnCalc.Click
+    Dim lTot As Integer
+    Dim lFraction As Double
+    Dim lStandard As Integer
+
+    Try
+      lStandard = CInt(TxtStand.Text)
+    Catch ex As Exception
+      lStandard = 0
+    End Try
+
+    lTot = 0
+    lTot = lTot + sCalcLine(0, TxtNumb0, TxtWidth0, LbTot0)
+    lTot = lTot + sCalcLine(1, TxtNumb1, TxtWidth1, LbTot1)
+    lTot = lTot + sCalcLine(2, TxtNumb2, TxtWidth2, LbTot2)
+    lTot = lTot + sCalcLine(3, TxtNumb3, TxtWidth3, LbTot3)
+    lTot = lTot + sCalcLine(4, TxtNumb4, TxtWidth4, LbTot4)
+    lTot = lTot + sCalcLine(5, TxtNumb5, TxtWidth5, LbTot5)
+    lTot = lTot + sCalcLine(6, TxtNumb6, TxtWidth6, LbTot6)
+    lTot = lTot + sCalcLine(7, TxtNumb7, TxtWidth7, LbTot7)
+    lTot = lTot + sCalcLine(8, TxtNumb8, TxtWidth8, LbTot8)
+    lTot = lTot + sCalcLine(9, TxtNumb9, TxtWidth9, LbTot9)
+
+    LbTot.Text = CStr(lTot)
+    If lStandard = 0 Then
+      lFraction = -1
+    Else
+      lFraction = lTot / lStandard
+    End If
+    LbFraction.Text = Format(lFraction, "#0.0000")
+  End Sub
+
+  Private Function sCalcLine(pLine As Integer, pNumber As TextBox, pWidth As TextBox, pTotal As Label) As Integer
+    Dim lNumber As Integer
+    Dim lWidth As Integer
+    Dim lTot As Integer
+
+    Try
+      lNumber = CInt(pNumber.Text)
+      lWidth = CInt(pWidth.Text)
+    Catch ex As Exception
+      lNumber = 0
+      lWidth = 0
+    End Try
+
+    lTot = lNumber * lWidth
+    pTotal.Text = CStr(lTot)
+
+    Return lTot
+  End Function
+
+End Class
